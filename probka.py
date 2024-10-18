b38e81c9fc79044ad9900dc3104d1a9800082f5b
ArtyomKeith <kekulya00@gmail.com>
2024-10-18 18:37:47 +0500

    Update probka.py

--- probka.py
+++ probka.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pydeck as pdk
-import io
 
 # Загружаем данные
 file_path = "aggregated_results.xlsx"  # Укажите путь к вашему файлу
@@ -63,101 +62,92 @@ if not filtered_data.empty:
     stats.columns = ['Страна', 'Среднее', 'Минимум', 'Максимум']  # Русские названия столбцов
     st.write(stats)
 
+    # Выбор цветов для графиков
+    color_palette = st.color_picker("Выберите цвет графиков", "#1f77b4")
+
+    # Выбор типа графика
+    plot_type = st.selectbox("Выберите тип графика:", ["Линейный", "Столбчатый"])
+
     # Настройка стиля
     plt.style.use('ggplot')  # Используем стиль ggplot, который встроен в Matplotlib
 
     # Вкладки для графиков
-    tab1, tab2 = st.tabs(["Линейный график", "Столбчатая диаграмма"])
+    tab1, tab2 = st.tabs(["График", "Карта"])
 
-    # Линейный график
+    # График
     with tab1:
-        st.write("График ниже показывает изменение {} по годам для выбранных стран.".format(metrics_mapping[selected_metric]))
-        plt.figure(figsize=(12, 6))
-        sns.lineplot(data=filtered_data, x='year', y=selected_metric, hue='Страна', marker='o', linewidth=2.5)
+        if plot_type == "Линейный":
+            st.write("График ниже показывает изменение {} по годам для выбранных стран.".format(metrics_mapping[selected_metric]))
+            plt.figure(figsize=(12, 6))
+            sns.lineplot(data=filtered_data, x='year', y=selected_metric, hue='Страна', marker='o', linewidth=2.5, palette=[color_palette])
 
-        # Настройка осей
-        plt.xticks(filtered_data['year'].unique(), rotation=45, fontsize=10)
-        plt.yticks(fontsize=10)
+            # Настройка осей
+            plt.xticks(filtered_data['year'].unique(), rotation=45, fontsize=10)
+            plt.yticks(fontsize=10)
 
-        # Добавляем заголовок и подписи к осям
-        plt.title('Изменение {} по годам'.format(metrics_mapping[selected_metric]), fontsize=16, weight='bold', color='darkblue')
-        plt.xlabel('Год', fontsize=12, color='darkgreen')
-        plt.ylabel(metrics_mapping[selected_metric], fontsize=12, color='darkgreen')
+            # Добавляем заголовок и подписи к осям
+            plt.title('Изменение {} по годам'.format(metrics_mapping[selected_metric]), fontsize=16, weight='bold', color='darkblue')
+            plt.xlabel('Год', fontsize=12, color='darkgreen')
+            plt.ylabel(metrics_mapping[selected_metric], fontsize=12, color='darkgreen')
 
-        # Настройка легенды
-        plt.legend(title='Страна', fontsize=10, title_fontsize=12, loc='upper right')
+            # Настройка легенды
+            plt.legend(title='Страна', fontsize=10, title_fontsize=12, loc='upper right')
 
-        # Добавляем сетку
-        plt.grid(True, which='both', linestyle='--', linewidth=0.7, color='gray', alpha=0.7)
+            # Добавляем сетку
+            plt.grid(True, which='both', linestyle='--', linewidth=0.7, color='gray', alpha=0.7)
 
-        # Подгонка макета
-        plt.tight_layout()
+            # Подгонка макета
+            plt.tight_layout()
 
-        # Отображаем линейный график в Streamlit
-        st.pyplot(plt)
+            # Отображаем линейный график в Streamlit
+            st.pyplot(plt)
 
-    # Столбчатая диаграмма
-    with tab2:
-        st.write("Столбчатая диаграмма для сравнения {} по странам.".format(metrics_mapping[selected_metric]))
-        plt.figure(figsize=(12, 6))
-        sns.barplot(data=filtered_data, x='year', y=selected_metric, hue='Страна', ci=None)
-        
-        # Настройка осей
-        plt.title('Сравнение {} по странам'.format(metrics_mapping[selected_metric]), fontsize=16, weight='bold', color='darkblue')
-        plt.xlabel('Год', fontsize=12, color='darkgreen')
-        plt.ylabel(metrics_mapping[selected_metric], fontsize=12, color='darkgreen')
-        plt.xticks(rotation=45, fontsize=10)
-        plt.yticks(fontsize=10)
-        plt.legend(title='Страна', fontsize=10, title_fontsize=12, loc='upper right')
+        elif plot_type == "Столбчатый":
+            st.write("Столбчатая диаграмма для сравнения {} по странам.".format(metrics_mapping[selected_metric]))
+            plt.figure(figsize=(12, 6))
+            sns.barplot(data=filtered_data, x='year', y=selected_metric, hue='Страна', ci=None, palette=[color_palette])
 
-        # Отображаем столбчатую диаграмму в Streamlit
-        st.pyplot(plt)
+            # Настройка осей
+            plt.title('Сравнение {} по странам'.format(metrics_mapping[selected_metric]), fontsize=16, weight='bold', color='darkblue')
+            plt.xlabel('Год', fontsize=12, color='darkgreen')
+            plt.ylabel(metrics_mapping[selected_metric], fontsize=12, color='darkgreen')
+            plt.xticks(rotation=45, fontsize=10)
+            plt.yticks(fontsize=10)
+            plt.legend(title='Страна', fontsize=10, title_fontsize=12, loc='upper right')
 
-    # Интерактивные пояснения
-    with st.expander("Интерактивные пояснения"):
-        st.write("""На графиках вы можете увидеть изменения выбранного показателя по странам и годам. 
-            Столбчатая диаграмма позволяет сравнить показатели между странами за выбранные годы.
-        """)
-
-    # Экспорт данных в Excel
-    st.write("Экспортировать отфильтрованные данные в Excel:")
-    if st.button("Скачать данные в Excel"):
-        buffer = io.BytesIO()
-        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            filtered_data.to_excel(writer, index=False, sheet_name='Данные')
-        
-        buffer.seek(0)  # Перемещаем указатель в начало буфера
-        st.download_button("Скачать", buffer, "filtered_data.xlsx")
+            # Отображаем столбчатую диаграмму в Streamlit
+            st.pyplot(plt)
 
     # Карта
-    st.write("Карта с расположением стран:")
-    map_data = pd.DataFrame({
-        'Страна': ['Казахстан', 'Кыргызстан', 'Таджикистан', 'Узбекистан'],
-        'Лат': [48.0196, 41.2044, 38.8610, 41.3775],
-        'Лон': [66.9237, 74.7661, 74.5698, 64.5850]
-    })
+    with tab2:
+        st.write("Карта с расположением стран:")
+        map_data = pd.DataFrame({
+            'Страна': ['Казахстан', 'Кыргызстан', 'Таджикистан', 'Узбекистан'],
+            'Лат': [48.0196, 41.2044, 38.8610, 41.3775],
+            'Лон': [66.9237, 74.7661, 74.5698, 64.5850]
+        })
 
-    # Добавляем карту
-    st.pydeck_chart(pdk.Deck(
-        initial_view_state=pdk.ViewState(
-            latitude=39.5,
-            longitude=66.9,
-            zoom=3,
-            pitch=0,
-        ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=map_data,
-                get_position='[Лон, Лат]',
-                get_color='[255, 0, 0]',
-                get_radius=50000,
-                pickable=True,
+        # Добавляем карту
+        st.pydeck_chart(pdk.Deck(
+            initial_view_state=pdk.ViewState(
+                latitude=39.5,
+                longitude=66.9,
+                zoom=3,
+                pitch=0,
             ),
-        ],
-        tooltip={
-            "text": "{Страна}\n{Лат}, {Лон}"
-        },
-    ))
+            layers=[
+                pdk.Layer(
+                    'ScatterplotLayer',
+                    data=map_data,
+                    get_position='[Лон, Лат]',
+                    get_color='[255, 0, 0]',
+                    get_radius=50000,
+                    pickable=True,
+                ),
+            ],
+            tooltip={
+                "text": "{Страна}\n{Лат}, {Лон}"
+            },
+        ))
 else:
     st.error("Не удалось получить данные для выбранных стран.")
